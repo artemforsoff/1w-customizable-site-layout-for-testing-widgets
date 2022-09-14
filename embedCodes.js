4407c9f41fa7b018be1f902c8872ba208482af60
Artem Sdobnikov <43861663+artemforsoff@users.noreply.github.com>
2022-09-14 16:48:28 +0300

    Add files via upload

---
+++ embedCodes.js
@@ -0,0 +1,15 @@
+export const embedCodes = {
+    'Old poller (desktop)': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="smart3"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Old poller (mobile)': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="smart3"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'New poller (desktop)': `<div id="widgetOWO" data-owo-code="{{id}}" data-owo-mode="poller"></div><script src="https://widget-frontend-qa.1worldonline.biz/constructor/index.js?ncu={{CLICK_URL_ESC}}" type="module" async></script>`,
+    'New poller (mobile)': `<div id="widgetOWO" data-owo-code="{{id}}" data-owo-mode="poller"></div><script src="https://widget-frontend-qa.1worldonline.biz/constructor/index.js?ncu={{CLICK_URL_ESC}}" type="module" async></script>`,
+    'Fact quiz': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="survey-knowledge-quiz"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Identity quiz': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="survey-character-quiz"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Debate': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="debate"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Survey (widget)': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="survey-study-mind"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Trivia': `<div data-owo-type="widget" data-owo-code="{{id}}" data-owo-mode="trivia"></div><script src="//frontend-qa.1worldonline.biz/poller-constructor.js?ncu={{CLICK_URL_ESC}}" type="text/javascript" async></script>`,
+    'Loyalty (widget)': `<div id="1WOLPWID" data-loyalty-widget-id="{{id}}"></div><script defer src="https://loyalty-qa-ecs.1worldonline.biz/widget-constructor.js"></script>`,
+    'Loyalty (tracker)': `<meta name="1WOLPWID-WIDGET-GUUD" content="{{id}}"><script defer src="https://loyalty-qa-ecs.1worldonline.biz/points-balance-widget.js"></script>`,
+    // 'Old survey (full page)': ``,
+    'New survey (full page)': `<div id="widgetOWO" data-owo-code="{{id}}" data-owo-locale="en" data-owo-mode="survey-full-page"></div><script src="https://widget-frontend-qa.1worldonline.biz/constructor/index.js?ncu={{CLICK_URL_ESC}}" type="module" async></script>`,
+};
